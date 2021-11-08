7d20571e5bcc3e45a464e3476f36d438444c83a5
Jacob Gardner <72172145+jgjg0799@users.noreply.github.com>
2021-11-08 09:59:48 -0700

    First Version of Arduino Code

---
+++ 0.0.1_-_11_8_21.ino
@@ -0,0 +1,40 @@
+//Arduino Code for Weather Station
+//Project 3; Jacob Gardner, Loren Larrieu, Ben Emerson, Mohammad Ahmad
+//Current Date: 11/8/21 9:56 AM
+
+#include <Servo.h>
+#include <LiquidCrystal.h>
+
+//variables for LED
+int greenLED = 13;
+int yellowLED = 12;
+int redLED = 11;
+//variables for Sensors
+int windSensor = A2;
+int tempSensor = A3;
+int levelSensor = A4;
+//variables for Servos
+int dumperPin = 9;
+int coverPin = 10;
+//variables for LCD Display
+int RS = 2;
+int E = 3;
+int D4 = 4;
+int D5 = 5;
+int D6 = 6; 
+int D7 = 7;
+//object creation
+LiquidCrystal lcd = (RS, E, D4, D5, D6, D7);    //lcd object
+Servo dumper;     //Servo object
+Servo cover;      //Servo object
+
+void setup() {
+  dumper.attach(dumperPin);     //link dumper to pin
+  cover.attach(coverPin);       //link cover to pin
+  lcd.begin(16,2)               //lcd column and rows
+}
+
+void loop() {
+  
+
+}
